dd181d71dadaf74d1c37df541a0675a07ad7ed19
brianmujjuni <brianmujjuni@engineer.com>
2024-03-05 11:45:07 +0300

    restrict prop data types using proptypes

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -11,10 +11,16 @@ const starContainerStyle = {
   display: "flex",
 };
 
-
-
-
-
+StarRating.propTypes={
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+  
+}
 export default function StarRating({ maxRating = 5,
     color = '#fcc419',
     size='48',className="",
