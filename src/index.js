53f943c6c2af8753e8bea0349f462d61df9d746a
brianmujjuni <brianmujjuni@engineer.com>
2024-03-05 16:16:21 +0300

    comment out the starRating component from index

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './components/StarRating';
-function Test(){
-  const [movieRating, setMovieRating] = useState(0)
-  return(
-    <div>
-      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating}/>
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-    
-  )
-}
+import './index.css';
+import App from './App';
+// import StarRating from './components/StarRating';
+
+
+
+
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     
-    <StarRating messages={['Terrible','Bad','Okay','Good','Amazing']} defaultRating={3}/>
-    <Test/>
+    {/* <StarRating messages={['Terrible','Bad','Okay','Good','Amazing']} defaultRating={3}/> */}
+   
   </React.StrictMode>
 );
 
