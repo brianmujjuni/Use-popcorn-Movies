4bb8c0bee6bfadf04028c02017a9a60446c99f27
brianmujjuni <brianmujjuni@engineer.com>
2024-03-15 12:28:25 +0300

    check for already rated movies

--- src/App.js
+++ src/App.js
@@ -126,6 +126,7 @@ export default function App() {
               selectedId={selectedId}
               onCloseMovie={handleCloseMovie}
               onAddWatched={handleAddWatched}
+              watched={watched}
             />
           ) : (
             <>
@@ -281,11 +282,11 @@ function ErrorMessage({ message }) {
   return <p className="error">{message}</p>;
 }
 
-function MovieDetails({ selectedId, onCloseMovie,onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie,onAddWatched,watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating,setUserRating] = useState('')
-
+  const isWatched = watched.map(movie => movie.imdbID).includes(selectedId)
   const {
     Title: title,
     Year: year,
@@ -353,8 +354,13 @@ function MovieDetails({ selectedId, onCloseMovie,onAddWatched }) {
           </header>
           <section>
             <div className="rating">
-              <StarRating maxRating={10} size={24} onSetRating={setUserRating}/>
+             {!isWatched ? 
+             <>
+               <StarRating maxRating={10} size={24} onSetRating={setUserRating}/> 
+              
+             
             {userRating > 0 && <button className="btn-add" onClick={handleAdd}>+ Add to list</button>}
+            </> : <p>You rated this movie</p>}
             </div>
             <p>
               <em>{plot}</em>
